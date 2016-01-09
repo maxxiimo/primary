f10d0bed59dffed32ee493ed7d9014f0c51bd0d4
Chris Maxwell <cmaxwell@ojala.com>
2016-01-08 16:29:54 -0800

    Add mobile boilerplate code.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,5 +7,4 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-# Rails.application.config.assets.precompile += %w( search.js )
-Rails.application.config.assets.precompile += %w( modernizr-2.8.3.min.js )
+Rails.application.config.assets.precompile += %w( modernizr-2.8.3.min.js mobile.css mobile.js )
