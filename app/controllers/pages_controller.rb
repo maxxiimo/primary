d8f13f4de083ad0725e954df6be3fddb8a07895d
Chris Maxwell <cmaxwell@ojala.com>
2016-01-22 12:33:22 -0800

    Add navigation and corresponding views for both desktop and mobile.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -5,6 +5,39 @@ class PagesController < ApplicationController
   def about
   end
 
+  def the_program
+  end
+
+  def is_this_for_me
+  end
+
+  def course_dates
+  end
+
+  def tuition
+  end
+
+  def info_sessions
+  end
+
+  def front_end_bootcamp
+  end
+
+  def part_time_courses
+  end
+
+  def launch_program
+  end
+
+  def employers
+  end
+
+  def blog
+  end
+
+  def apply
+  end
+
   def contact
   end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -3,5 +3,16 @@ Rails.application.routes.draw do
 
   get 'home',     to: 'pages#home',    as: :home
   get 'about',    to: 'pages#about',   as: :about
+  get 'the-program',    to: 'pages#the_program',   as: :program
+  get 'is-this-for-me',  to: 'pages#is_this_for_me', as: :for_me
+  get 'course-dates',  to: 'pages#course_dates', as: :schedule
+  get 'tuition',  to: 'pages#tuition', as: :tuition
+  get 'info-sessions',  to: 'pages#info_sessions', as: :info
+  get 'front-end-bootcamp',  to: 'pages#front_end_bootcamp', as: :front_end
+  get 'part-time-courses',  to: 'pages#part_time_courses', as: :part_time
+  get 'launch-program',  to: 'pages#launch_program', as: :launch
+  get 'employers',  to: 'pages#employers', as: :employer
+  get 'blog',  to: 'pages#blog', as: :blog
+  get 'apply',  to: 'pages#apply', as: :apply
   get 'contact',  to: 'pages#contact', as: :contact
 end
