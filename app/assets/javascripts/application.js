94f7d0b6abd5b0a4b2e182291c5dec0e01354c68
Chris Maxwell <cmaxwell@ojala.com>
2016-02-05 02:03:28 -0800

    Add Gridster.

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -13,4 +13,5 @@
 //= require jquery
 //= require jquery_ujs
 //= require turbolinks
+//= require jquery.gridster.min
 //= require custom
--- app/assets/javascripts/custom.js
+++ app/assets/javascripts/custom.js
@@ -1,4 +1,16 @@
 // ===========================
-// Enter Script Title Here
+// Gridster
 // ===========================
 
+$(function(){ //DOM Ready
+
+    $(".gridster ul").gridster({
+        widget_margins: [10, 10],
+        widget_base_dimensions: [300, 300],
+        avoid_overlapped_widgets: true,
+        resize: {
+            enabled: true
+        }
+    });
+
+});
