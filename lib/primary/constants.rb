2dd97aba2ab3c908e4dd22e6e2a48b8fb92bebab
Chris Maxwell <cmaxwell@ojala.com>
2016-03-13 21:24:46 -0700

    Move repeated constant to new constants.rb file. DRY principle.

---
+++ lib/primary/constants.rb
@@ -0,0 +1,4 @@
+# VALID_EMAIL_REGEX = /\A[\w+\-.]+@[a-z\d\-.]+\.[a-z]+\z/i
+# VALID_EMAIL_REGEX = /\A([\w\.%\+\-]+)@([\w\-]+\.)+([\w]{2,})\z/i
+# More options: http://www.regular-expressions.info/email.html
+VALID_EMAIL_REGEX = /^[-a-z0-9_+\.]+\@([-a-z0-9]+\.)+[a-z0-9]{2,4}$/i
