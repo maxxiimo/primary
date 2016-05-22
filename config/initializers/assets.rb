d34850d1ce19b25ea70296c7951cb9294a5fae64
Chris Maxwell <cmaxwell@ojala.com>
2016-05-22 00:45:04 -0700

    Reverse previous, causing error.

--- config/initializers/assets.rb
+++ config/initializers/assets.rb
@@ -7,4 +7,5 @@ Rails.application.config.assets.version = '1.0'
 
 # Precompile additional assets.
 # application.js, application.css, and all non-JS/CSS in app/assets folder are already added.
-Rails.application.config.assets.precompile += %w( modernizr-custom.js phone.css phone.js )
+Rails.application.config.assets.precompile += %w( modernizr-2.8.3.min.js phone.css phone.js )
+# Rails.application.config.assets.precompile += %w( modernizr-custom.js phone.css phone.js )
